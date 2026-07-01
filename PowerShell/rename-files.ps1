76c1ec638cdd4497bf46c3babef4f84a01aa3f5e
Brad <33629813+dvlprlife@users.noreply.github.com>
2026-07-01 16:03:08 -0400

    Use LiteralPath in rename-files script

--- PowerShell/rename-files.ps1
+++ PowerShell/rename-files.ps1
@@ -20,34 +20,29 @@ Function rename-files {
         [bool]$includeFolders = $false,
         [bool]$echo = $false
     )
-
-    if (Test-Path -Path $path) {
-        Get-ChildItem -path $path | Foreach-Object {
+    if (Test-Path -LiteralPath $path) {
+        Get-ChildItem -LiteralPath $path | Foreach-Object {
             $currentItem = $_
-
             if ((($currentItem -is [System.IO.DirectoryInfo]) -and $includeFolders) -or ($currentItem -is [System.IO.FileInfo])) {
                 $newName = $currentItem.Name -replace '[^A-Za-z0-9- \.\[\]]', ' '
                 $newName = $newName.ToLower() -replace ' ', '-'
-
                 if ($currentItem.Name -cne $newName) {
                     if ($echo) {
                         Write-Output "renaming: $($currentItem.Name)  to: $($newName)"
                     }
-
-                    # if it is a directory and only changing case an error will occur
-                    # rename to a temp filename and rename it to the lowercase name
-                    if ($currentItem.Name -ceq $newName) {
+                    # names match ignoring case => only the casing changed.
+                    # rename to a temp name first, then to the target, to avoid
+                    # "Source and destination path must be different"
+                    if ($currentItem.Name -eq $newName) {
                         $randomname = [System.IO.Path]::GetRandomFileName()
-                        $tempItem = Rename-Item -Path $currentItem.FullName -NewName $randomname -PassThru
-                        $currentItem = Rename-Item -Path $tempItem.FullName -NewName $newName -PassThru
+                        $tempItem    = Rename-Item -LiteralPath $currentItem.FullName -NewName $randomname -PassThru
+                        $currentItem = Rename-Item -LiteralPath $tempItem.FullName    -NewName $newName    -PassThru
                     }
                     else {
-                        $currentItem = Rename-Item -Path $currentItem.FullName -NewName $newName -PassThru
+                        $currentItem = Rename-Item -LiteralPath $currentItem.FullName -NewName $newName -PassThru
                     }
-
                 }
             }
-
             if ($currentItem -is [System.IO.DirectoryInfo]) {
                 rename-files $currentItem.FullName $includeFolders $echo
             }
